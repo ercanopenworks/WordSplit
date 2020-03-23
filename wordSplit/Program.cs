22251c3c2cbb56d4589029934b889fae2c55d0f0
Ercan-home <ercanerdogan@gmail.com>
2020-03-23 04:27:34 +0300

    solution has been completed

---
+++ wordSplit/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace wordSplit
+{
+    class Program
+    {
+        public static string WordSplit(string source, string[] strArr)
+        {
+
+            // code goes here  
+            string result="";
+
+            foreach (var item in strArr)
+            {
+                if (item.Length>2 && source.Contains(item))
+                {
+                    result += item + ",";
+                }
+
+            }
+
+            return result;
+
+        }
+        static void Main(string[] args)
+        {
+            //string[] itemArr = Array.ConvertAll(Console.ReadLine().ToString().Split(','), aTemp => Convert.ToString(aTemp));
+            string source = "baseball";
+            string words=  "a,all,b,ball,bas,base,cat,code,d,e,quit,z";
+            string[] itemArr = Array.ConvertAll(words.Split(','), aTemp => Convert.ToString(aTemp));
+
+            Console.WriteLine(WordSplit(source, itemArr));
+        }
+    }
+}
